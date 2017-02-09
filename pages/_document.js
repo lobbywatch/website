aef69309d0075b34187eb9b9029701743ab54768
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-02-10 00:19:03 +0100

    add fav icons

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, {Head, Main, NextScript} from 'next/document'
+import {LW_BLUE_LIGHT, WHITE} from '../src/theme'
 
 import {renderStatic} from 'glamor/server'
 
@@ -19,6 +20,14 @@ export default class MyDocument extends Document {
           <meta name='viewport' content='width=device-width,initial-scale=1' />
           <meta http-equiv='X-UA-Compatible' content='IE=edge' />
           {css ? <style dangerouslySetInnerHTML={{ __html: css }} /> : null}
+          <link rel='apple-touch-icon' sizes='180x180' href='/static/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' href='/static/favicon-32x32.png' sizes='32x32' />
+          <link rel='icon' type='image/png' href='/static/favicon-16x16.png' sizes='16x16' />
+          <link rel='manifest' href='/static/manifest.json' />
+          <link rel='mask-icon' href='/static/safari-pinned-tab.svg' color={LW_BLUE_LIGHT} />
+          <link rel='shortcut icon' href='/static/favicon.ico' />
+          <meta name='msapplication-config' content='/static/browserconfig.xml' />
+          <meta name='theme-color' content={WHITE} />
         </Head>
         <body>
           <Main />
