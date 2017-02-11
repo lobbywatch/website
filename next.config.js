49668c9848adc99a6f40d73952dfa3e04f8f0d48
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-02-11 23:27:22 +0100

    add babel polyfill for IE11

---
+++ next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  webpack: (config) => {
+    const entryFactory = config.entry;
+    config.entry = () => (
+      entryFactory()
+        .then((entry) => {
+          entry['main.js'] = [
+            'babel-polyfill',
+            entry['main.js'],
+          ];
+          return entry;
+        })
+    );
+    return config;
+  },
+};
