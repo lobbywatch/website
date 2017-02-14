d2ec8fe89e2aced3bf2c1e65d0b9344e17fa6eee
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-02-14 18:14:53 +0100

    integrate MailChimp

--- constants.js
+++ constants.js
@@ -8,3 +8,10 @@ const hostname = location ? location.hostname : '127.0.0.1'
 const port = location ? location.port : exports.EXPRESS_PORT
 const protocol = location ? location.protocol : 'http:'
 exports.GRAPHQL_URI = `${protocol}//${hostname}${port ? `:${port}` : ''}/graphql`
+
+exports.MAILCHIMP_BASE_URL = 'https://lobbywatch.us8.list-manage.com'
+exports.MAILCHIMP_U = 'd27da194665399ad4c9acc6fd'
+exports.MAILCHIMP_ID_FOR_LOCALE = {
+  de: '82c011426c',
+  fr: 'b5a3964333'
+}
--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, {Component} from 'react'
+import jsonp from '../utils/jsonp'
+import qs from 'querystring'
 
 import {withT} from './Message'
-import {Strong, Input, Submit} from './Styled'
+import {Strong, Input, Submit, P} from './Styled'
 import {css} from 'glamor'
+import {MAILCHIMP_BASE_URL, MAILCHIMP_U, MAILCHIMP_ID_FOR_LOCALE} from '../../constants'
 
 const flexIconsStyle = css({
   display: 'flex',
   justifyContent: 'space-between',
   width: '100%',
   marginTop: 10,
-  '& input[type=text]': {
+  '& input[type=email], & input[type=text]': {
     width: 'auto',
     flexGrow: 2
   },
@@ -20,14 +23,65 @@ const flexIconsStyle = css({
   }
 })
 
-const Newsletter = ({locale, t}) => (
-  <div>
-    <Strong>{t('newsletter/title')}</Strong>
-    <form {...flexIconsStyle}>
-      <Input type='text' placeholder={t('newsletter/placeholder')} />
-      <Submit value={t('newsletter/submit')} />
-    </form>
-  </div>
-)
+class Newsletter extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      email: '',
+      message: null
+    }
+  }
+  onSubmit (event) {
+    event.preventDefault()
+
+    const {t, locale} = this.props
+    const {email} = this.state
+    const query = {
+      EMAIL: email,
+      u: MAILCHIMP_U,
+      id: MAILCHIMP_ID_FOR_LOCALE[locale],
+      hl: 'de'
+    }
+    jsonp(`${MAILCHIMP_BASE_URL}/subscribe/post-json?${qs.encode(query)}`, {param: 'c'}, (error, data) => {
+      if (error) {
+        this.setState({message: t('newsletter/error/timeout')})
+        return
+      }
+
+      if (data.result !== 'success') {
+        let message = data.msg
+          ? data.msg.replace(/^[0-9]+ - /, '')
+          : t('newsletter/error/generic')
+        if (
+          message.indexOf('is already subscribed') > -1 ||
+          message.indexOf('est déjà abonné') > -1 ||
+          message.indexOf('schon im Verteiler') > -1
+        ) {
+          message = t('newsletter/error/already-subscribed')
+        }
+        this.setState({message})
+      } else {
+        this.setState({message: t('newsletter/succes'), email: ''})
+      }
+    })
+  }
+  render () {
+    const {message, email} = this.state
+    const {t, locale} = this.props
+
+    return (
+      <div>
+        <Strong>{t('newsletter/title')}</Strong>
+        <form {...flexIconsStyle} target='_blank' onSubmit={e => this.onSubmit(e)} action={`${MAILCHIMP_BASE_URL}/subscribe/post`} method='POST'>
+          <input type='hidden' name='u' value={MAILCHIMP_U} />
+          <input type='hidden' name='id' value={MAILCHIMP_ID_FOR_LOCALE[locale]} />
+          <Input type='email' name='EMAIL' placeholder={t('newsletter/placeholder')} onChange={e => this.setState({email: e.target.value})} value={email} />
+          <Submit value={t('newsletter/submit')} />
+        </form>
+        {!!message && <P>{message}</P>}
+      </div>
+    )
+  }
+}
 
 export default withT(Newsletter)
