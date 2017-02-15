5cb7b79cdc2c8efad2a718f73ff79d59fa94c204
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-02-15 12:06:19 +0100

    support hover in global styles

--- src/utils/css.js
+++ src/utils/css.js
@@ -1,22 +1,25 @@
 import {css, cssFor} from 'glamor'
 
+const getCssText = (selector, close, styles) => {
+  const className = css(styles).toString()
+  const mediaCss = cssFor(styles)
+  const expectedSelector = `.${className},[data-${className}]{`
+  const actualSelector = mediaCss.slice(0, expectedSelector.length)
+  if (actualSelector !== expectedSelector) {
+    throw new Error(`Expected glamor selector «${expectedSelector}» got «${actualSelector}»\nCSS:\n${mediaCss}`)
+  }
+  return selector +
+    mediaCss.replace(expectedSelector, '') +
+    close
+}
+
 export const globalWithMediaQueries = (selector, styles) => {
   const plainStyles = {}
   Object.keys(styles).forEach(key => {
     if (key.indexOf('@media') === 0) {
-      const mediaStyle = styles[key]
-      const className = css(mediaStyle).toString()
-      const mediaCss = cssFor(mediaStyle)
-      const expectedSelector = `.${className},[data-${className}]{`
-      const actualSelector = mediaCss.slice(0, expectedSelector.length)
-      if (actualSelector !== expectedSelector) {
-        throw new Error(`Expected glamor selector «${expectedSelector}» got «${actualSelector}»\nCSS:\n${mediaCss}`)
-      }
-      css.insert(
-        `${key}{${selector}{` +
-        mediaCss.replace(expectedSelector, '') +
-        '}'
-      )
+      css.insert(getCssText(`${key}{${selector}{`, '}', styles[key]))
+    } else if (key === ':hover') {
+      css.insert(getCssText(`${selector}${key}{`, '', styles[key]))
     } else {
       plainStyles[key] = styles[key]
     }
