3a8c52442586b31c554c46f1bc40f69b45fc058b
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-02-16 12:16:04 +0100

    fix expand icon

--- src/assets/Expand.js
+++ src/assets/Expand.js
@@ -3,7 +3,7 @@ import {LW_BLUE, WHITE} from '../theme'
 
 const Icon = ({size, style, className, color, expanded}) => (
   <svg width={size} height={size} style={style} className={className} viewBox='0 0 24 24'>
-    <circle stroke={color} strokeWidth='2' fill={WHITE} cx='12' cy='12' r='11' />
+    <circle stroke={color} strokeWidth='1' fill={WHITE} cx='12' cy='12' r='11' />
     {!expanded && <path fill={color} d='M11 6h2v12h-2z' />}
     <path fill={color} d='M6 11h12v2H6z' />
   </svg>
