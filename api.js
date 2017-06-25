edaaa69a123c14c4791c626c5312aed2bc0063c9
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-06-25 21:30:31 +0200

    lint

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 
 const graphql = require('./graphql')
-const PORT = typeof process !== 'undefined' && process.env.PORT || 5000
+const PORT = (typeof process !== 'undefined' && process.env.PORT) || 5000
 
 const server = express()
 
