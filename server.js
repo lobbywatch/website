4adfa17d919c87b3a584e5dd550e81f56f0c406c
Thomas Preusse <tpreusse@users.noreply.github.com>
2018-04-21 23:46:53 +0200

    add throng

--- server.js
+++ server.js
@@ -1,55 +1,64 @@
-const express = require('express')
-const next = require('next')
-const acceptLanguage = require('accept-language')
+const throng = require('throng')
 
 const DEV = process.env.NODE_ENV !== 'production'
 if (DEV || process.env.DOTENV) {
   require('dotenv').config()
 }
+const WEB_CONCURRENCY = process.env.WEB_CONCURRENCY || 1
 
-const graphql = require('./graphql')
-const {
-  locales,
-  EXPRESS_PORT,
-  GOOGLE_SITE_VERIFICATION,
-  PUBLIC_BASE_URL
-} = require('./constants')
-const routes = require('./routes')
+const start = (workerId) => {
+  const express = require('express')
+  const next = require('next')
+  const acceptLanguage = require('accept-language')
+  const graphql = require('./graphql')
+  const {
+    locales,
+    EXPRESS_PORT,
+    GOOGLE_SITE_VERIFICATION,
+    PUBLIC_BASE_URL
+  } = require('./constants')
+  const routes = require('./routes')
 
-const app = next({dir: '.', dev: DEV})
-const handler = routes.getRequestHandler(app)
+  const app = next({dir: '.', dev: DEV})
+  const handler = routes.getRequestHandler(app)
 
-acceptLanguage.languages(locales)
+  acceptLanguage.languages(locales)
 
-app.prepare().then(() => {
-  const server = express()
+  app.prepare().then(() => {
+    const server = express()
 
-  graphql(server)
+    graphql(server)
 
-  if (!DEV && PUBLIC_BASE_URL) {
-    server.enable('trust proxy')
-    server.use((req, res, next) => {
-      if (`${req.protocol}://${req.get('Host')}` !== PUBLIC_BASE_URL) {
-        return res.redirect(PUBLIC_BASE_URL + req.url)
-      }
-      return next()
+    if (!DEV && PUBLIC_BASE_URL) {
+      server.enable('trust proxy')
+      server.use((req, res, next) => {
+        if (`${req.protocol}://${req.get('Host')}` !== PUBLIC_BASE_URL) {
+          return res.redirect(PUBLIC_BASE_URL + req.url)
+        }
+        return next()
+      })
+    }
+
+    server.get('/', (req, res) => {
+      res.redirect(`/${acceptLanguage.get(req.headers['accept-language'])}`)
     })
-  }
+    server.get(`/${routes.localeSegment}/search/daten/:term?`, (req, res) => {
+      const {locale, term} = req.params
+      res.redirect(301, `/${encodeURIComponent(locale)}/search${term ? `?term=${encodeURIComponent(term)}` : ''}`)
+    })
+    server.get(`/${GOOGLE_SITE_VERIFICATION}`, (req, res) => {
+      res.send(`google-site-verification: ${GOOGLE_SITE_VERIFICATION}`)
+    })
+    server.use(handler)
 
-  server.get('/', (req, res) => {
-    res.redirect(`/${acceptLanguage.get(req.headers['accept-language'])}`)
+    server.listen(EXPRESS_PORT, (err) => {
+      if (err) throw err
+      console.log(`[${workerId}] Listening on ${EXPRESS_PORT}`)
+    })
   })
-  server.get(`/${routes.localeSegment}/search/daten/:term?`, (req, res) => {
-    const {locale, term} = req.params
-    res.redirect(301, `/${encodeURIComponent(locale)}/search${term ? `?term=${encodeURIComponent(term)}` : ''}`)
-  })
-  server.get(`/${GOOGLE_SITE_VERIFICATION}`, (req, res) => {
-    res.send(`google-site-verification: ${GOOGLE_SITE_VERIFICATION}`)
-  })
-  server.use(handler)
+}
 
-  server.listen(EXPRESS_PORT, (err) => {
-    if (err) throw err
-    console.log(`> Ready on port ${EXPRESS_PORT}`)
-  })
-})
+throng({
+  workers: WEB_CONCURRENCY,
+  lifetime: Infinity
+}, start)
