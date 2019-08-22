d18c0f7d5f4fbb36bdbbe07c6a1989802f40138d
Roland Kurmann <info@scito.ch>
2019-08-22 08:15:58 +0200

    Update graphql/mappers.js
    Improve setting of active value
    
    Co-Authored-By: Thomas Preusse <tpreusse@users.noreply.github.com>

--- graphql/mappers.js
+++ graphql/mappers.js
@@ -330,7 +330,7 @@ const mapParliamentarian = exports.mapParliamentarian = (raw, t) => {
       }
     } : null,
     canton: raw.kanton_name,
-    active: raw.aktiv,
+    active: !!+raw.aktiv,
     council: raw.ratstyp,
     councilTitle: () => {
       return t(
