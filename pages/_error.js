3f94e52013f57c0d2e66988f85ba531312999f18
Thomas Preusse <tpreusse@users.noreply.github.com>
2021-04-26 20:49:00 +0200

    add custom error page

---
+++ pages/_error.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import {useRouter} from 'next/router'
+
+import Frame, {Center} from 'src/components/Frame'
+import MetaTags from 'src/components/MetaTags'
+import {H1, P} from 'src/components/Styled'
+
+import {getSafeLocale} from '../constants'
+
+function Error({ statusCode, errorMessage }) {
+  const {query: {locale: queryLocale}} = useRouter()
+  const locale = getSafeLocale(queryLocale)
+
+  return (
+    <Frame>
+      <MetaTags locale={locale} title={statusCode || 'Unbekannter Fehler'} />
+      <Center>
+        <H1>Unerwarteter Fehler</H1>
+        <P>Wir bitten um Entschuldigung. Versuchen Sie die Seite nochmals neuzuladen?</P>
+        <H1>Erreur inattendue</H1>
+        <P>Nous demandons des excuses. Essayez-vous à nouveau de recharger la page?</P>
+        {(!!errorMessage || !!statusCode) && <>
+          <H1>Status Code {statusCode || 'Unkown'}</H1>
+          <P>{errorMessage}</P>
+        </>}
+      </Center>
+    </Frame>
+  )
+}
+
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : undefined
+  if (res) { // server only
+    console.error('[error]', res.statusCode, err)
+  }
+  return {
+    statusCode,
+    errorMessage: err?.toString()
+  }
+}
+
+export default Error
