4c5f9a62911c16c0ad9a871262b38a844ce47a42
Thomas Preusse <tpreusse@users.noreply.github.com>
2022-08-29 14:04:42 +0200

    purposes

--- pages/[locale]/index.js
+++ pages/[locale]/index.js
@@ -6,14 +6,17 @@ import { useRouter } from 'next/router'
 import Loader from 'src/components/Loader'
 import Frame, { Center } from 'src/components/Frame'
 import MetaTags from 'src/components/MetaTags'
-import Message from 'src/components/Message'
+import Message, { useT } from 'src/components/Message'
 import Card from 'src/components/Card'
 import Grid, { GridItem } from 'src/components/Grid'
-import { H2, StyledLink } from 'src/components/Styled'
+import { H2, H3, P, StyledLink } from 'src/components/Styled'
 
 import { createGetStaticProps } from 'lib/apolloClientSchemaLink'
 import { locales } from '../../constants'
 import SearchField from 'src/components/Frame/SearchField'
+import { PurposeList, PurposeItem } from 'src/components/Purpose'
+import { typeSegments } from 'src/utils/routes'
+import { intersperse } from 'src/utils/helpers'
 
 const indexQuery = gql`
   query index($locale: Locale!) {
@@ -40,6 +43,7 @@ const Index = () => {
       locale: locale,
     },
   })
+  const t = useT(locale)
 
   return (
     <Frame landing>
@@ -56,6 +60,31 @@ const Index = () => {
               })}
             />
             <Center>
+              <PurposeList>
+                <PurposeItem>
+                  <H3>Wir recherchieren Interessenskonflikte</H3>
+                  <P>
+                    Politik muss transparent sein. Deshalb schauen wir, für
+                    welche Unternehmen und Organisationen sich Nationalrätinnen
+                    und Ständeräte engagieren.
+                  </P>
+                </PurposeItem>
+                <PurposeItem>
+                  <H3>Wir sind unabhängig</H3>
+                  <P>
+                    Lobbywatch ist eine unabhängige journalistische Plattform.
+                    Wir sind gemeinnützig – finanziert durch Mitglieder,
+                    Stiftungen und Gönner:innen.
+                  </P>
+                </PurposeItem>
+                <PurposeItem>
+                  <H3>Werden Sie Lobbywatcher:in</H3>
+                  <P>
+                    Werden Sie Mitglied von Lobbywatch und engagieren Sie sich
+                    so für eine lebendige und transparente Demokratie.
+                  </P>
+                </PurposeItem>
+              </PurposeList>
               <H2>
                 <Message id='index/blog/title' locale={locale} />
               </H2>
@@ -75,6 +104,29 @@ const Index = () => {
                 <Message id='index/search/title' locale={locale} />
               </H2>
               <SearchField />
+              <P>
+                {intersperse(
+                  [
+                    {
+                      label: t('menu/parliamentarians'),
+                      href: `/${locale}/${typeSegments.Parliamentarian}`,
+                    },
+                    {
+                      label: t('menu/guests'),
+                      href: `/${locale}/${typeSegments.Guest}`,
+                    },
+                    {
+                      label: t('menu/lobbygroups'),
+                      href: `/${locale}/${typeSegments.LobbyGroup}`,
+                    },
+                  ].map(({ label, href }) => (
+                    <StyledLink key={href} href={href}>
+                      {label}
+                    </StyledLink>
+                  )),
+                  ' – '
+                )}
+              </P>
             </Center>
           </div>
         )}
---
+++ src/components/Purpose.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { css } from 'glamor'
+
+const PADDING = 20
+const mediaColumns = '@media only screen and (min-width: 640px)'
+
+const styles = {
+  list: css({
+    listStyle: 'none',
+    margin: `15px -${PADDING}px 30px`,
+    padding: 0,
+    display: 'flex',
+    flexWrap: 'wrap',
+  }),
+  item: css({
+    display: 'block',
+    margin: '0 auto',
+    padding: `0 ${PADDING}px`,
+    width: '100%',
+    [mediaColumns]: {
+      width: '33%',
+      textAlign: 'center',
+    },
+    '& > h3': {
+      marginBottom: 5,
+    },
+    '& > p': {
+      marginTop: 0,
+    },
+  }),
+}
+
+export const PurposeList = ({ children }) => {
+  return <ul {...styles.list}>{children}</ul>
+}
+
+export const PurposeItem = ({ children }) => {
+  return <li {...styles.item}>{children}</li>
+}
