87ada2332edf05af9828499a0b8cb865f4961503
Peter Gassner <peter@gassner.io>
2025-10-25 18:29:35 +0200

    refact: Move src/utils to TS

--- src/utils/formats.ts
+++ src/utils/formats.ts
@@ -10,24 +10,16 @@ const swissNumbers = formatLocale({
 export const numberFormat = swissNumbers.format(',')
 const chf4Format = swissNumbers.format('$.0f')
 const chf5Format = swissNumbers.format('$,.0f')
-export const chfFormat = (value) => {
+export const chfFormat = (value: number) => {
   if (String(Math.round(value)).length > 4) {
     return chf5Format(value)
   }
   return chf4Format(value)
 }
-const count4Format = swissNumbers.format('.0f')
-const count5Format = swissNumbers.format(',.0f')
-export const countFormat = (value) => {
-  if (String(Math.round(value)).length > 4) {
-    return count5Format(value)
-  }
-  return count4Format(value)
-}
 
 /** Converts dd.mm.yyyy to yyyy-mm-dd. Ignores time */
-export const convertDateToIso = (date) => {
+export const convertDateToIso = (date?: string): string => {
   return date
     ? date.replace(/^([0-2]\d|3[01])\.(0\d|1[0-2])\.(\d{4}).*$/, '$3-$2-$1')
-    : {}
+    : ''
 }
