ebc664fee4e020e7ee365e4d5e271a81cb480eff
Peter Gassner <peter@gassner.io>
2025-10-29 22:16:01 +0100

    feat: Re-introduce purpose (wip)

--- pages/[locale]/index.tsx
+++ pages/[locale]/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useRouter } from 'next/router'
-import Frame from 'src/components/Frame'
-import { useT } from 'src/components/Message'
+import Frame, { Center } from 'src/components/Frame'
+import Message, { useT } from 'src/components/Message'
 
 import MetaTags from 'src/components/MetaTags'
 import { getSafeLocale, PUBLIC_BASE_URL } from '../../constants'
+import { H1, H2, P } from '../../src/components/Styled'
 
 const Page = () => {
   const router = useRouter()
@@ -20,6 +21,36 @@ const Page = () => {
         description={t('index/meta/description')}
         image={`${PUBLIC_BASE_URL}/static/social/index.png`}
       />
+      <Center>
+        <H1>
+          <Message id={'claim'} locale={locale} />
+        </H1>
+
+        <P>
+          <Message id={'index/meta/description'} locale={locale} />
+        </P>
+
+        <H2>
+          <Message id={'purpose/research/title'} locale={locale} />
+        </H2>
+        <P>
+          <Message id={'purpose/research/text'} locale={locale} />
+        </P>
+
+        <H2>
+          <Message id={'purpose/independence/title'} locale={locale} />
+        </H2>
+        <P>
+          <Message id={'purpose/independence/text'} locale={locale} />
+        </P>
+
+        <H2>
+          <Message id={'purpose/nonprofit/title'} locale={locale} />
+        </H2>
+        <P>
+          <Message id={'purpose/nonprofit/text'} locale={locale} />
+        </P>
+      </Center>
     </Frame>
   )
 }
