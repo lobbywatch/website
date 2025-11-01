ceba92208538606a4cb2519c2f2782de3db41543
Peter Gassner <peter@gassner.io>
2025-11-01 12:16:04 +0100

    refact: Cleanup constants

--- constants.js
+++ constants.js
@@ -6,13 +6,7 @@ export const DRUPAL_DATA_BASE_URL = 'https://data.lobbywatch.ch'
 export const IMAGE_BASE_URL =
   'https://www.parlament.ch/sitecollectionimages/profil/portrait-260/'
 
-export const SERVER_PORT = (typeof process !== 'undefined' && ENV.PORT) || 3000
-
-export const NEXT_PUBLIC_VERCEL_URL = ENV.NEXT_PUBLIC_VERCEL_URL
-export const PUBLIC_BASE_URL =
-  ENV.PUBLIC_BASE_URL ||
-  ENV.NEXT_PUBLIC_BASE_URL ||
-  (NEXT_PUBLIC_VERCEL_URL ? `https://${NEXT_PUBLIC_VERCEL_URL}` : undefined)
+export const PUBLIC_BASE_URL = ENV.PUBLIC_BASE_URL
 
 export const MATOMO_URL_BASE = ENV.MATOMO_URL_BASE
 export const MATOMO_SITE_ID = ENV.MATOMO_SITE_ID
