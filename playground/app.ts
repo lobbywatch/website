cd80670297cc63a5faa51e333c63444ee1be2797
Peter Gassner <peter@gassner.io>
2025-11-11 19:30:20 +0100

    feat: Use custom webserver

---
+++ playground/app.ts
@@ -0,0 +1,81 @@
+import type { RequestListener } from 'node:http'
+import path from 'node:path'
+import fsp from 'node:fs/promises'
+import fs from 'node:fs'
+import type { Locale } from '../src/domain'
+
+type MimeType = keyof typeof MIME_TYPES
+
+const MIME_TYPES = {
+  default: 'application/octet-stream',
+  html: 'text/html; charset=UTF-8',
+  js: 'text/javascript',
+  css: 'text/css',
+  png: 'image/png',
+  jpg: 'image/jpeg',
+  gif: 'image/gif',
+  ico: 'image/x-icon',
+  svg: 'image/svg+xml',
+}
+
+const DIST_PATH = path.join(process.cwd(), './dist')
+
+const toBool = [() => true, () => false]
+
+const prepareFile = async (
+  url: string,
+): Promise<{ mimeType: string; stream: fs.ReadStream } | undefined> => {
+  // FIXME Security, access to FS?
+  const paths = [DIST_PATH, url]
+  if (url.endsWith('/')) paths.push('index.html')
+  const filePath = path.join(...paths)
+  const found = await fsp.access(filePath).then(...toBool)
+  if (found) {
+    const ext = path.extname(filePath).substring(1).toLowerCase() as MimeType
+    const mimeType = MIME_TYPES[ext] ?? MIME_TYPES['default']
+    const stream = fs.createReadStream(filePath)
+    return { mimeType, stream }
+  } else {
+    return undefined
+  }
+}
+
+export interface SSRModule {
+  render: (locale: Locale) => string
+}
+
+export interface AppEnv {
+  loadBundle: (name: string) => Promise<SSRModule>
+  loadHtml: (name: string) => Promise<string>
+}
+
+export const app =
+  ({ loadBundle, loadHtml }: AppEnv, next: VoidFunction): RequestListener =>
+  async (req, res) => {
+    if (req.url === '/') {
+      res.writeHead(302, { Location: '/de' })
+      res.end()
+    } else if (req.url === '/de') {
+      const importedApp = await loadBundle('index')
+      const markup = importedApp.render('de')
+      const template = await loadHtml('index')
+      const html = template.replace(`<!--app-content-->`, markup)
+      res.writeHead(200, { 'Content-Type': 'text/html' })
+      res.end(html)
+    } else if (req.url === '/test') {
+      const importedApp = await loadBundle('test')
+      const markup = importedApp.render('de')
+      const template = await loadHtml('test')
+      const html = template.replace(`<!--app-content-->`, markup)
+      res.writeHead(200, { 'Content-Type': 'text/html' })
+      res.end(html)
+    } else {
+      const file = req.url != null ? await prepareFile(req.url) : undefined
+      if (file) {
+        res.writeHead(200, { 'Content-Type': file.mimeType })
+        file.stream.pipe(res)
+      } else {
+        next()
+      }
+    }
+  }
---
+++ rsbuild.config.ts
@@ -0,0 +1,72 @@
+import {
+  defineConfig,
+  logger,
+  type RequestHandler,
+  type SetupMiddlewaresContext,
+} from '@rsbuild/core'
+import { pluginReact } from '@rsbuild/plugin-react'
+import { app, type AppEnv, type SSRModule } from './playground/app.ts'
+
+export const serverRender = (
+  serverContext: SetupMiddlewaresContext,
+): RequestHandler => {
+  const appEnv: AppEnv = {
+    loadBundle: async (name: string) =>
+      await serverContext.environments.node.loadBundle<SSRModule>(name),
+    loadHtml: async (name: string) =>
+      await serverContext.environments.web.getTransformedHtml(name),
+  }
+
+  return async (req, res, next) => {
+    app(appEnv, next)(req, res)
+  }
+}
+
+export default defineConfig({
+  plugins: [pluginReact()],
+  dev: {
+    writeToDisk: true,
+    setupMiddlewares: [
+      ({ unshift }, serverContext) => {
+        const serverRenderMiddleware = serverRender(serverContext)
+        unshift(async (req, res, next) => {
+          try {
+            await serverRenderMiddleware(req, res, next)
+          } catch (err) {
+            logger.error('SSR render error, downgrade to CSR...')
+            logger.error(err)
+            next()
+          }
+        })
+      },
+    ],
+  },
+  html: {
+    template: './playground/template.html',
+  },
+  environments: {
+    web: {
+      source: {
+        entry: {
+          index: './playground/index.tsx',
+          test: './playground/test.tsx',
+        },
+      },
+    },
+    node: {
+      source: {
+        entry: {
+          index: './playground/index.server.tsx',
+          test: './playground/test.server.tsx',
+        },
+      },
+      output: {
+        module: true,
+        target: 'node',
+        distPath: {
+          root: 'dist/server',
+        },
+      },
+    },
+  },
+})
