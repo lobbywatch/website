06c3fc328b095b6b5491a3d873548a91a553ce8c
Peter Gassner <peter@gassner.io>
2025-12-30 17:55:40 +0100

    feat: Add statistics to landing page

--- pages/[locale]/index.tsx
+++ pages/[locale]/index.tsx
@@ -6,9 +6,11 @@ import MetaTags from 'src/components/MetaTags'
 import { PUBLIC_BASE_URL } from '../../constants'
 import { useLocale } from '../../src/vendor/next'
 
+import styles from './index.module.css'
+import { typeSegments } from '../../src/utils/routes.ts'
+
 const Page = () => {
   const locale = useLocale()
-
   const t = useT(locale)
 
   return (
@@ -19,38 +21,86 @@ const Page = () => {
         description={t('index/meta/description')}
         image={`${PUBLIC_BASE_URL}/static/social/index.png`}
       />
-      <div className='u-center-container'>
-        <h1>
-          <Message id={'claim'} locale={locale} />
-        </h1>
+      <div className={styles.container}>
+        <div>
+          <h1>
+            <Message id={'index/title'} locale={locale} />
+          </h1>
 
-        <p>
-          <Message id={'index/meta/description'} locale={locale} />
-        </p>
+          <p className={styles.lead}>
+            <Message id={'index/byline'} locale={locale} raw />
+          </p>
+        </div>
+        <div className={styles.factsGrid}>
+          <PrimaryCard
+            title={t('menu/parliamentarians')}
+            count={244}
+            url={`/${locale}/${typeSegments.Parliamentarian}`}
+          />
+          <PrimaryCard
+            title={t('menu/lobbygroups')}
+            count={139}
+            url={`/${locale}/${typeSegments.LobbyGroup}`}
+          />
+          <PrimaryCard
+            title={t('menu/guests')}
+            count={1331}
+            url={`/${locale}/${typeSegments.Guest}`}
+          />
+        </div>
 
-        <h2>
-          <Message id={'purpose/research/title'} locale={locale} />
-        </h2>
-        <p>
-          <Message id={'purpose/research/text'} locale={locale} />
-        </p>
+        <FlourishCard
+          chartId={'26738457'}
+          title='Transparenzquote 2025 (%)'
+          height={220}
+        />
 
-        <h2>
-          <Message id={'purpose/independence/title'} locale={locale} />
-        </h2>
-        <p>
-          <Message id={'purpose/independence/text'} locale={locale} />
-        </p>
-
-        <h2>
-          <Message id={'purpose/nonprofit/title'} locale={locale} />
-        </h2>
-        <p>
-          <Message id={'purpose/nonprofit/text'} locale={locale} />
-        </p>
+        <FlourishCard
+          chartId={'26990045'}
+          title='Transparenzquote nach Fraktionen (%)'
+          height={400}
+        />
       </div>
     </Frame>
   )
 }
 
 export default Page
+
+// -------------------------------------------------------------------------------------------------
+
+interface PrimaryCardProps {
+  title: string
+  count: number
+  url: string
+}
+
+const PrimaryCard = ({ title, count, url }: PrimaryCardProps) => (
+  <div className={styles.primaryCard}>
+    <h2>{title}:</h2>
+    <p>{count}</p>
+    <a href={url}>Alle entdecken</a>
+  </div>
+)
+
+interface FlourishCardProps {
+  chartId: string
+  title: string
+  height: number
+}
+
+const FlourishCard = ({ chartId, title, height }: FlourishCardProps) => (
+  <div className={styles.flourishCard}>
+    <iframe
+      src={`https://flo.uri.sh/visualisation/${chartId}/embed`}
+      title={title}
+      className='flourish-embed-iframe'
+      style={{
+        width: '100%',
+        height,
+        border: 'none',
+      }}
+      sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'
+    ></iframe>
+  </div>
+)
