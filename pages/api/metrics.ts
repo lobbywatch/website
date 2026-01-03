e4a2ccd3691d500b157228b04099fec69000f14e
Björn Mosler <bjorndown@smoler.ch>
2026-01-03 10:34:20 +0100

    add auth to metrics endpoint

--- pages/api/metrics.ts
+++ pages/api/metrics.ts
@@ -1,13 +1,25 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { collectDefaultMetrics, register } from 'prom-client';
+import { collectDefaultMetrics, register } from 'prom-client'
 
-collectDefaultMetrics();
+collectDefaultMetrics()
+const BEARER_TOKEN = process.env.METRICS_BEARER_TOKEN
 
-export default async function handler(_request: NextApiRequest, res: NextApiResponse) {
-try {
-    res.setHeader('Content-Type', register.contentType);
-    res.end(await register.metrics());
+export default async function handler(
+  request: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (
+    request.headers.authorization !== `Bearer ${BEARER_TOKEN}` ||
+    !BEARER_TOKEN
+  ) {
+    res.status(404).end()
+    return
+  }
+
+  try {
+    res.setHeader('Content-Type', register.contentType)
+    res.end(await register.metrics())
   } catch (err) {
-    res.status(500).end(err);
+    res.status(500).end(err)
   }
 }
